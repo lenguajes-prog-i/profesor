872e37374dc29a6199f6ebf9ab1d36e5f0b0e502
KrozFu <carlosuefagh@gmail.com>
2026-03-24 19:37:08 -0500

    feat: cliente-servidor

---
+++ cliente-servidor/servidor.py
@@ -0,0 +1,26 @@
+import socket
+
+HOST = "127.0.0.1"  
+PORT = 5000 
+
+# socket TCP
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# IP - Perto
+server.bind((HOST, PORT))
+
+server.listen()
+
+print("Servidor esperando conexión...")
+
+conn, addr = server.accept()
+print(f"Conexión establecida desde {addr}")
+
+data = conn.recv(1024).decode()
+print(f"Mensaje recibido: {data}")
+
+respuesta = "Mensaje recibido correctamente"
+conn.send(respuesta.encode())
+
+conn.close()
+server.close()
